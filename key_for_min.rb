fd69bcda4674a6d99598ea6339df8386782cf42e
Michael Maggs <michaelmaggs@gmail.com>
2019-05-17 23:53:35 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,7 @@ def key_for_min_value(name_hash)
   name_hash.each do |key, value|
     if name_hash.length == 0 
       return nil
-    elsif name_hash[key] < min_value
+    elsif name_hash[key] < min_value || min_value == nil
       min_key = key
     end
   end
